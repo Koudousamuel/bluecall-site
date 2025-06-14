e45d985823dad53c8d80380a1109836e107dd3fa
Koudousamuel <bluecall57@gmail.com>
2025-06-14 10:32:55 +0000

    Correction de DATABASES avec conn_max_age

--- monprojet/settings.py
+++ monprojet/settings.py
@@ -79,16 +79,18 @@ WSGI_APPLICATION = 'monprojet.wsgi.application'
 
 if os.environ.get('RENDER'):  # Quand le site est en ligne
     DATABASES = {
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600
+        )
     }
-else:  # Quand tu travailles sur ton PC
+else:  # Quand tu travailles en local
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
