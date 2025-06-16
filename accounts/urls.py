b92b262c5dc3279dd6d4006c3ee503573980fbad
Koudousamuel <bluecall57@gmail.com>
2025-06-16 14:45:53 +0000

    Correction erreur test_view non défini

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('mot-de-passe-oublie/', views.demander_reinitialisation, name='password_reset_request'),
     path('verification-code/', views.verifier_code_reset, name='verify_reset_code'),
     path('nouveau-mot-de-passe/', views.nouveau_mot_de_passe, name='set_new_password'),
-    path('test/', test_view),
+    path('test/', views.test_view),
     
 
 ]
