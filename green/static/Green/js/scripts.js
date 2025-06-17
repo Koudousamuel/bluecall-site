e0f3933dc2f2f30e5c6311d5c877c499559e0403
Koudousamuel <bluecall57@gmail.com>
2025-06-17 18:45:00 +0000

    Mise à jour du CSS et du js

--- green/static/Green/js/scripts.js
+++ green/static/Green/js/scripts.js
@@ -1,4 +1,3 @@
-
 function filtrerProduits(categorie) {
     let data = {};
 
@@ -19,21 +18,25 @@ function filtrerProduits(categorie) {
             } else {
                 data.forEach(product => {
                     container.append(`
-                        <div class="col-md-2 gy-9 gx-0">
-                            <div class="card">
-                                <img src="${product.image_url}" alt="" class="card-img-top" height="280">
-                                <div class="card-body">
-                                    <div class="card-title" id="aa${product.id}">${product.title}</div>
-                                    <div class="card-text"><span id="price${product.id}"  class="text-danger">${product.price} FCFA</span></div>
-                                    <a href="/${product.id}/" class="btn btn-warning">Voir</a>   
-                                    <button 
-                                    class="btn ted btn-custom" 
-                                    data-id="${ product.id }" 
-                                    data-nom="${product.title}">
-
-                                    Ajouter
-                                    </button>
-              
+                        <div class="col-6 col-sm-4 col-md-2 mb-2">
+                            <div class="card h-100">
+                                <img src="${product.image_url}" alt="" class="card-img-top" style="height: 180px; object-fit: cover;">
+                                <div class="card-body p-2">
+                                    <div class="card-title text-truncate" id="aa${product.id}" title="${product.title}">
+                                        ${product.title}
+                                    </div>
+                                    <div class="card-text">
+                                        <span id="price${product.id}" class="text-danger">${product.price} FCFA</span>
+                                    </div>
+                                    <div class="mt-2 d-flex flex-wrap gap-1">
+                                        <a href="/${product.id}/" class="btn btn-warning btn-sm flex-fill">Voir</a>   
+                                        <button 
+                                            class="btn ted btn-custom btn-sm flex-fill" 
+                                            data-id="${product.id}" 
+                                            data-nom="${product.title}">
+                                            Ajouter
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
