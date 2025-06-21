8c75eacd532380d2bc5f6de36cd93e859bb69483
Koudousamuel <bluecall57@gmail.com>
2025-06-21 14:01:48 +0000

    Suppression du module resend

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,6 @@ from .forms import CustomUserCreationForm
 from django.core.mail import send_mail
 from django.http import HttpResponse
 import random
-import resend
 from django.contrib.auth.models import User
 from .forms import PasswordResetRequestForm
 from .forms import NouveauMotDePasseForm
