e48202b2d2cfe2d6b9ad32a7c6200855320cc05e
Koudousamuel <bluecall57@gmail.com>
2025-08-03 18:40:02 +0000

    Fix: ajout de l'import Path pour BASE_DIR

--- monprojet/urls.py
+++ monprojet/urls.py
@@ -18,10 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from green.views import create_admin
 
 urlpatterns = [
-    path('creer-admin/', create_admin),
     path('admin/', admin.site.urls),
     path('', include('green.urls')),
     path('accounts/', include('accounts.urls'))
